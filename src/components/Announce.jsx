42b4f66942b20a5a6053f3817c4b569f655d1156
ChefDimas <dimalunik@aol.com>
2022-03-17 18:15:13 +0200

    Finished Announcer

--- src/components/Announce.jsx
+++ src/components/Announce.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Close } from "@material-ui/icons";
+import { React, useState } from "react";
 
 function Announce() {
-  return <div>Announce</div>;
+  const [announceStyle, setAnnounceStyle] = useState(
+    "bg-[#8a4af3] font-bold text-white flex items-center justify-center"
+  );
+
+  const handleClose = () => {
+    setAnnounceStyle(announceStyle + " hidden");
+  };
+
+  return (
+    <div className={announceStyle}>
+      <h2>Sale 40% off</h2>
+      <Close className="cursor-pointer" onClick={handleClose} />
+    </div>
+  );
 }
 
 export default Announce;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar h-[60px] shadow-md relative" z-100>
+      {/* Wrapper of NavBar */}
+      <div className="wrapper pl-[20px] pr-[20px] pt-[10px] pb-[10px] flex justify-between items-center">
+        {/* Left Div */}
+        <div className="left flex flex-1 items-center">
+          <div className="language cursor-pointer text-[16px]">EN</div>
+        </div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;

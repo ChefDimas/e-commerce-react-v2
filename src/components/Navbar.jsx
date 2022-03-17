37a3d64327ab8efd246f3e5c01f6e41a315ac6c3
ChefDimas <dimalunik@aol.com>
2022-03-17 18:24:03 +0200

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Search } from "@material-ui/icons";
 import React from "react";
 
 function Navbar() {
@@ -8,6 +9,12 @@ function Navbar() {
         {/* Left Div */}
         <div className="left flex flex-1 items-center">
           <div className="language cursor-pointer text-[16px]">EN</div>
+          {/* Search Input */}
+          <div className="searchingInput flex border-2 border-solid border-lightgrey rounded-md items-center ml-[10px] p-[5px]">
+            <input type="text" className="input outline-none" />
+            {/* Material UI Icon */}
+            <Search />
+          </div>
         </div>
         <div>2</div>
         <div>3</div>

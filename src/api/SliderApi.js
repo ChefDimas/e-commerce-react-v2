f4b0c24b9d66804a65994ab10631e231f5fee2b6
ChefDimas <dimalunik@aol.com>
2022-03-18 14:54:30 +0200

    Create slideBar

---
+++ src/api/SliderApi.js
@@ -0,0 +1,18 @@
+export const SliderApi = [
+  {
+    src: "https://images.unsplash.com/photo-1603189343302-e603f7add05a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    content: {
+      h2: "Amazing Deals this Summer",
+      p: "Up to 40% off",
+    },
+    background: "white",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1538329972958-465d6d2144ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    content: {
+      h2: "Hurry Up Now",
+      p: "Up to 40% off",
+    },
+    background: "#ffd028",
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Announce from "../components/Announce";
 import Navbar from "../components/Navbar";
+import Slidebar from "../components/Slidebar";
 
 function Home() {
   return (
     <div>
       <Announce />
       <Navbar />
+      <Slidebar />
     </div>
   );
 }

b0fca515595e8eb0c91d0f00373dddc6a8c4d13a
ChefDimas <dimalunik@aol.com>
2022-03-22 22:43:31 +0200

    changes

---
+++ src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Counter() {
+  return (
+    <div>
+      <div className="flex items-center justify-center text-2xl">
+        Quantity
+        <div>-</div>
+        <div>1</div>
+        <div>+</div>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;

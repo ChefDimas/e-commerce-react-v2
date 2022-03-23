02cdbfbd3fcfdc848b6a312e03283d28043c851a
ChefDimas <dimalunik@aol.com>
2022-03-23 19:38:10 +0200

    Counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,9 +5,15 @@ function Counter() {
     <div>
       <div className="flex items-center justify-center text-2xl">
         Quantity
-        <div>-</div>
-        <div>1</div>
-        <div>+</div>
+        <div className="ml-5 shadow-md flex">
+          <div className="bg-[#8a4af3] text-white w-8 flex items-center justify-center cursor-pointer rounded-l-md">
+            -
+          </div>
+          <div>1</div>
+          <div className="bg-[#8a4af3] text-white w-8 flex items-center justify-center cursor-pointer">
+            +
+          </div>
+        </div>
       </div>
     </div>
   );

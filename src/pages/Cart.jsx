b8ae1333382c281530dc22f514b5abe39c76cc5e
ChefDimas <dimalunik@aol.com>
2022-03-25 00:19:01 +0200

    Finished cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,61 +3,168 @@ import Announce from "../components/Announce";
 import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
-
-function Cart() {
-  //Number of items const
-  const itemsQuantity = 1;
+import Counter from "../components/Counter";
+const Cart = () => {
+  const SummaryItemStyle = "SummaryItem flex justify-between mt-3 w-[100%]";
+  const ProductDivStyle = "flex w-[100%] h-auto items-center mobile:flex-col";
+  const PriceQuantityStyle =
+    "flex-auto flex flex-col justify-center items-center mobile:mt-7 mobile:mb-7";
   return (
     <div>
       <Announce />
       <Navbar />
-      <div className="p-5">
+      <div className="p-3">
         <div className="flex justify-center text-5xl">Cart</div>
-        <div className="flex items-center justify-between mt-4">
-          <button className="btn bg-white text-[#8a4af3] border-2 border-[#8a4af3] hover:bg-white">
+
+        {/* upper buttons div */}
+        <div className="flex items-center justify-between mt-4 mobile:flex-col">
+          <button className="btn bg-white text-[#8a4af3] border-2 border-[#8a4af3] mt-0">
             Continue Shopping
           </button>
-          <div className="flex underline text-lg hover:cursor-pointer">
-            <p>Items in your cart : {itemsQuantity}</p>
+          <div className="flex underline text-lg hover:cursor-pointer mobile:m-5">
+            <p>Items in your Cart: 3</p>
+            <p className="ml-5">WhishList Items: 0</p>
           </div>
-          <button className="btn">Checkout</button>
+          <button className="btn mt-0">Checkout Now</button>
         </div>
-        {/* Central Div */}
-        <div className="flex mt-7">
-          <div className="flex flex-col flex-1 bg-blue-200">
-            {/* List of products div */}
-            <div className="flex w-[100%] h-auto items-center bg-yellow-500">
-              <div className="product flex self-start pl-5">
+
+        {/* vertically center parent div */}
+        <div className="flex flex-row mt-7 mobile:flex-col">
+          {/* product div */}
+          <div className="flex flex-col flex-1">
+            {/* 1st product div */}
+            <div className={ProductDivStyle}>
+              <div className=" product flex pl-5 self-start">
                 <img
-                  className="w-[80%] h-[80%] rounded-lg shadow-lg hover:scale-1.1 ease-in duration-300"
-                  src="https://www.c-and-a.com/productimages/b_rgb:EBEBEB,c_scale,h_860,q_auto:eco,e_sharpen:70/v1643022569/2170303-1-08.jpg"
+                  className="product_img w-[7rem]"
+                  src="https://cdn.shopify.com/s/files/1/0240/7285/products/WithinYourselfLongSleeveT-ShirtinElectricBlue04_360x.jpg?v=1642719824"
                   alt="product_img"
                 />
-                <div>
+
+                <div className="disc flex items-start justify-between h-auto flex-col ml-6">
                   <p>
-                    <b>ID</b> : 12345
+                    <b className="mr-2">ID:</b>123456789
                   </p>
                   <p>
-                    <b>Product</b> : Harry Potter Hoodie
+                    <b className="mr-2">Product:</b>Dazzing Sky Shirt
+                  </p>
+                  <p className="flex items-center justify-center">
+                    <b className="mr-2">Color:</b>
+                    <div className="colorSelect bg-sky-500 ml-1 w-[20px] h-[20px]"></div>
                   </p>
                   <p>
-                    <b>Color</b> : Light-Blue
-                  </p>
-                  <p>
-                    <b>Size</b> : Medium
+                    <b className="mr-2">Size:</b>M
                   </p>
                 </div>
               </div>
-              <div className="bg-green-400">Product quantity and price</div>
+
+              {/*Price and Quantity Div*/}
+              <div className={PriceQuantityStyle}>
+                <Counter />
+                <p className="flex items-center justify-center text-4xl mt-3">
+                  <b>70$</b>
+                </p>
+              </div>
+            </div>
+
+            <hr className="mb-7 mt-7 mobile:mt-0" />
+            {/* 2nd product div */}
+            <div className={ProductDivStyle}>
+              <div className=" product flex pl-5 self-start">
+                <img
+                  className="product_img w-[7rem]"
+                  src="https://cdn.shopify.com/s/files/1/0240/7285/products/WithinYourselfLongSleeveT-ShirtinElectricBlue04_360x.jpg?v=1642719824"
+                  alt="product_img"
+                />
+
+                <div className="disc flex items-start justify-between h-auto flex-col ml-6">
+                  <p>
+                    <b className="mr-2">ID:</b>123456789
+                  </p>
+                  <p>
+                    <b className="mr-2">Product:</b>Dazzing Sky Shirt
+                  </p>
+                  <p className="flex items-center justify-center">
+                    <b className="mr-2">Color:</b>
+                    <div className="colorSelect bg-sky-500 ml-1 w-[20px] h-[20px]"></div>
+                  </p>
+                  <p>
+                    <b className="mr-2">Size:</b>M
+                  </p>
+                </div>
+              </div>
+
+              {/*Price and Quantity Div*/}
+              <div className={PriceQuantityStyle}>
+                <Counter />
+                <p className="flex items-center justify-center text-4xl mt-3">
+                  <b>70$</b>
+                </p>
+              </div>
+            </div>
+            <hr className="mb-7 mt-7 mobile:mt-0" />
+
+            {/* 3rd Product */}
+            <div className={ProductDivStyle}>
+              <div className=" product flex pl-5 self-start">
+                <img
+                  className="product_img w-[7rem]"
+                  src="https://cdn.shopify.com/s/files/1/0240/7285/products/WithinYourselfLongSleeveT-ShirtinElectricBlue04_360x.jpg?v=1642719824"
+                  alt="product_img"
+                />
+
+                <div className="disc flex items-start justify-between h-auto flex-col ml-6">
+                  <p>
+                    <b className="mr-2">ID:</b>123456789
+                  </p>
+                  <p>
+                    <b className="mr-2">Product:</b>Dazzing Sky Shirt
+                  </p>
+                  <p className="flex items-center justify-center">
+                    <b className="mr-2">Color:</b>
+                    <div className="colorSelect bg-sky-500 ml-1 w-[20px] h-[20px]"></div>
+                  </p>
+                  <p>
+                    <b className="mr-2">Size:</b>M
+                  </p>
+                </div>
+              </div>
+
+              {/*Price and Quantity Div*/}
+              <div className={PriceQuantityStyle}>
+                <Counter />
+                <p className="flex items-center justify-center text-4xl mt-3">
+                  <b>70$</b>
+                </p>
+              </div>
+            </div>
+            <hr className="mb-7 mt-7 mobile:mt-0" />
+          </div>
+          <div className="Summary flex-[0.4] flex flex-col items-center w-auto h-[40vh] border-2 border-[#8a4af3] rounded-md shadow-lg p-5 text-lg mobile:mb-6">
+            <h1 className="text-[2rem]">SUMMARY</h1>
+            <div className={SummaryItemStyle}>
+              <p>SubTotal:</p>
+              <p>$140</p>
+            </div>
+            <div className={SummaryItemStyle}>
+              <p>Shipping:</p>
+              <p>$10</p>
+            </div>
+            <div className={SummaryItemStyle}>
+              <p>Shipping Discount:</p>
+              <p>-$10</p>
+            </div>
+            <div className={SummaryItemStyle + " text-3xl font-bold"}>
+              <p>Total:</p>
+              <p>$140</p>
             </div>
           </div>
-          <div className="bg-red-400">Summary</div>
         </div>
+        <Newsletter />
+        <Footer />
       </div>
-      <Newsletter />
-      <Footer />
     </div>
   );
-}
+};
 
 export default Cart;
